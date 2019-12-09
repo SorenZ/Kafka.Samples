6e220a382c1f55b8e507163e5ef983e846240a8f
Hamed ZVand <sunrise.freedom@gmail.com>
2019-12-09 11:38:38 +0330

    upgrade consumer to new kafka API 1.3.0

--- src/Kafka.Consumer/Startup.cs
+++ src/Kafka.Consumer/Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace Kafka.Consumer
 {
@@ -16,26 +15,16 @@ namespace Kafka.Consumer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IHostedService, KafkaConsumer>();
+            services.AddHostedService<KafkaService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Consumer started");
-                });
-            });
         }
     }
 }
